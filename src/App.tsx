317209b2252256841c9cec34036d2197af9e2dee
1eshiy <1eshiy@vk.com>
2025-10-22 19:15:12 +0700

    install and initialize tanstack-router

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,29 @@ import CommunitiesPage from "./pages/CommunitiesPage.tsx";
 import FriendsPage from "./pages/FriendsPage.tsx";
 import ProfilePage from "./pages/ProfilePage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "./api";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/schedule" element={<SchedulePage />} />
-                <Route path="/mail" element={<MailPage />} />
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/schedule" element={<SchedulePage />} />
+                    <Route path="/mail" element={<MailPage />} />
 
-                <Route path="/friends" element={<FriendsPage />} />
+                    <Route path="/friends" element={<FriendsPage />} />
 
-                <Route path="/messenger/:id?" element={<MessengerPage />} />
-                <Route path="/communities/:id?" element={<CommunitiesPage />} />
+                    <Route path="/messenger/:id?" element={<MessengerPage />} />
+                    <Route path="/communities/:id?" element={<CommunitiesPage />} />
 
-                <Route path="/profile/:id?" element={<ProfilePage />} />
-                <Route path="/" element={<Navigate to="/profile" />} />
+                    <Route path="/profile/:id?" element={<ProfilePage />} />
+                    <Route path="/" element={<Navigate to="/profile" />} />
 
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-        </BrowserRouter>
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     )
 }
 
