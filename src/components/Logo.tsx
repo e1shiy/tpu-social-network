ef61cab30003e9f288fea38babc6bd6f12379227
1eshiy <1eshiy@vk.com>
2025-11-30 22:59:05 +0700

    add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import LogoIcon from "../assets/images/logo/logo-dark_ru.svg?react"
+
+function Logo() {
+    return (
+        <a href="https://tpu.ru">
+            <LogoIcon className="h-[2.1875rem] sm:h-[2.35rem] md:h-[2.5rem] lg:h-[2.7rem] xl:h-[2.85rem] 2xl:h-[3.125rem]" />
+        </a>
+    )
+}
+
+export default Logo
