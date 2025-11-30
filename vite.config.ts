e79c0c44272390addab813b73f051515adbeeba2
1eshiy <1eshiy@vk.com>
2025-11-30 22:57:21 +0700

    project setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),
+      svgr({
+      svgrOptions: {
+        dimensions: false, // отключает добавление width и height
+        svgProps: {
+          // "stroke-width": "2"
+        }
+      },
+    })
   ],
 })
