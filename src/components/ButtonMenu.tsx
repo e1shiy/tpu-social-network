1c107b05d7057500374352dfa4b3864a0f84cab7
1eshiy <1eshiy@vk.com>
2025-12-09 22:12:12 +0700

    add ButtonMenu component

---
+++ src/components/ButtonMenu.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx"
+import { twMerge } from "tailwind-merge"
+import Button, {type ButtonProps} from "./Button.tsx";
+
+interface ButtonMenuProps {
+    buttons: ButtonProps[],
+    className?: string,
+}
+
+function ButtonMenu({buttons, className} : ButtonMenuProps) {
+    return (
+        <ul className={twMerge(clsx(
+            `rounded-md md:rounded-lg lg:rounded-[0.625rem] 
+            p-2 md:p-2.5 flex flex-col gap-1.5 md:gap-2.5 bg-primary`,
+            className
+        ))}>
+            {buttons.map((button, i) => <Button {...button} key={i}/>)}
+        </ul>
+    )
+}
+
+export default ButtonMenu
