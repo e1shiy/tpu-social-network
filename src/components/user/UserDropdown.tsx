f9bf91ca6a69b50cbf8c1c5902cf93fdde8dd566
1eshiy <1eshiy@vk.com>
2025-12-11 00:08:28 +0700

    add fade in/out dropdown animations

--- src/components/user/UserDropdown.tsx
+++ src/components/user/UserDropdown.tsx
@@ -1,6 +1,5 @@
-import {useState} from "react"
-import UserDropdownButton from "./UserDropdownButton"
 import ButtonMenu from "../ButtonMenu.tsx"
+import ChevronRight from "../../assets/images/chevron-right.svg?react"
 import avatarUrl from "../../assets/images/image.png"
 import UserIcon from "../../assets/images/user.svg?react"
 import FriendsIcon from "../../assets/images/two_users.svg?react"
@@ -11,13 +10,16 @@ import {useNavHandlers} from "../../hooks/useNavHandlers.ts";
 import isCorrectLocation from "../../utils/isCorrectLocation.ts";
 import {useLocation} from "react-router-dom";
 import {FRIENDS_ROUTE, PROFILE_ROUTE} from "../../constants/routes.ts";
+import DropdownButton, {type ContentProps, type TriggerProps} from "../DropdownButton.tsx";
+import {twMerge} from "tailwind-merge";
+import clsx from "clsx";
+import Button from "../Button.tsx";
 
 interface UserDropdown {
     userId: string,
 }
 
 function UserDropdown({userId}: UserDropdown) {
-    const [isActive, setIsActive] = useState<boolean>(false)
     const {onFriendsClick, onProfileClick}: NavHandlers = useNavHandlers()
     const location = useLocation()
 
@@ -25,30 +27,52 @@ function UserDropdown({userId}: UserDropdown) {
     const userName = "Веретнов Алексей"
     const userAvatarUrl = avatarUrl
 
-    return (
-        <div className="relative">
-            <UserDropdownButton
-                className={"max-sm:w-max"}
-                avatarUrl={userAvatarUrl}
-                isActive={isActive}
-                onClick={() => setIsActive(i => !i)}
-            >
-                <span className={"max-sm:hidden text-ellipsis text-nowrap overflow-hidden"}>{userName}</span>
-            </UserDropdownButton>
+    const Trigger = ({isActive, onClick, className}: TriggerProps) => <Button
+        className={twMerge(clsx(
+            `py-[0.3125rem] sm:py-[0.32rem] md:py-[0.33rem] lg:py-[0.345rem] xl:py-[0.36rem] 2xl:py-[0.375rem] 
+            max-sm:w-max`, className
+        ))}
+        onClick={onClick}
+    >
+        <img src={userAvatarUrl} className="aspect-square h-full rounded-full shrink-0" alt="avatar"/>
+        <span className={"max-sm:hidden text-ellipsis text-nowrap overflow-hidden"}>{userName}</span>
+        <ChevronRight className={twMerge(clsx(
+            `h-4 sm:h-4.5 md:h-5 lg:h-5.3 xl:h-5.6 2xl:h-6 aspect-square text-current transition-transform shrink-0`,
+            isActive && "rotate-90"
+        ))}/>
+    </Button>
 
-            {isActive && <ButtonMenu
-                className={`
-                absolute top-full translate-y-1.25 md:translate-y-1.75 xl:translate-y-2.5 w-full
-                rounded-md md:rounded-lg lg:rounded-[0.625rem] min-w-max right-0
-                `}
-                buttons={[
-                    {children: "Профиль", TrailingIcon: UserIcon, onClick: () => onProfileClick(userId), isActive: isCorrectLocation(PROFILE_ROUTE(userId), location)},
-                    {children: "Друзья", TrailingIcon: FriendsIcon, onClick: onFriendsClick, isActive: isCorrectLocation(FRIENDS_ROUTE, location)},
-                    {children: "Настройки", TrailingIcon: SettingsIcon, onClick: () => { /* TODO: settings */ }, isActive: false }, // TODO path check
-                    {children: "Выйти из аккаунта", TrailingIcon: LogoutIcon, onClick: () => { /* TODO: logout */}, isActive: false}, // TODO path check
-                ]}/>
-            }
-        </div>
+    const Content = ({className, ref}: ContentProps) => <ButtonMenu
+        className={twMerge(clsx(
+            `rounded-md md:rounded-lg lg:rounded-[0.625rem] min-w-max right-0`, className
+        ))}
+        ref={ref}
+        buttons={[
+            {
+                children: "Профиль",
+                TrailingIcon: UserIcon,
+                onClick: () => onProfileClick(userId),
+                isActive: isCorrectLocation(PROFILE_ROUTE(userId), location)
+            },
+            {
+                children: "Друзья",
+                TrailingIcon: FriendsIcon,
+                onClick: onFriendsClick,
+                isActive: isCorrectLocation(FRIENDS_ROUTE, location)
+            },
+            {
+                children: "Настройки", TrailingIcon: SettingsIcon, onClick: () => { /* TODO: settings */
+                }, isActive: false
+            }, // TODO path check
+            {
+                children: "Выйти из аккаунта", TrailingIcon: LogoutIcon, onClick: () => { /* TODO: logout */
+                }, isActive: false
+            }, // TODO path check
+        ]}
+    />
+
+    return (
+        <DropdownButton Trigger={Trigger} Content={Content}/>
     )
 }
 
--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,3 +1,1 @@
 export { default as UserDropdown } from "./UserDropdown"
-export { default as UserDropdownButton } from "./UserDropdownButton"
-export { default as DropdownMenu } from "../ButtonMenu.tsx"
