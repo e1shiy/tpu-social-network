a3c780f1482f7cb75c5c68c0e26c0b418f93e9d0
1eshiy <1eshiy@vk.com>
2025-12-10 16:50:36 +0700

    adapt UserDropdown.tsx to using isCorrectLocation

--- src/components/user/UserDropdown.tsx
+++ src/components/user/UserDropdown.tsx
@@ -8,6 +8,9 @@ import SettingsIcon from "../../assets/images/settings.svg?react"
 import LogoutIcon from "../../assets/images/log-out.svg?react"
 import type {NavHandlers} from "../../providers/NavHandlersProvider.tsx";
 import {useNavHandlers} from "../../hooks/useNavHandlers.ts";
+import isCorrectLocation from "../../utils/isCorrectLocation.ts";
+import {useLocation} from "react-router-dom";
+import {FRIENDS_ROUTE, PROFILE_ROUTE} from "../../constants/routes.ts";
 
 interface UserDropdown {
     userId: string,
@@ -16,6 +19,7 @@ interface UserDropdown {
 function UserDropdown({userId}: UserDropdown) {
     const [isActive, setIsActive] = useState<boolean>(false)
     const {onFriendsClick, onProfileClick}: NavHandlers = useNavHandlers()
+    const location = useLocation()
 
     // todo user info
     const userName = "Веретнов Алексей"
@@ -32,12 +36,15 @@ function UserDropdown({userId}: UserDropdown) {
             </UserDropdownButton>
 
             {isActive && <ButtonMenu
-                className={"absolute top-full translate-y-1.25 md:translate-y-1.75 xl:translate-y-2.5 w-full"}
+                className={`
+                absolute top-full translate-y-1.25 md:translate-y-1.75 xl:translate-y-2.5 w-full
+                rounded-md md:rounded-lg lg:rounded-[0.625rem] min-w-max right-0
+                `}
                 buttons={[
-                    {children: "Профиль", TrailingIcon: UserIcon, onClick: () => onProfileClick(userId)},
-                    {children: "Друзья", TrailingIcon: FriendsIcon, onClick: onFriendsClick},
-                    {children: "Настройки", TrailingIcon: SettingsIcon, onClick: () => { /* TODO: settings */ }},
-                    {children: "Выйти из аккаунта", TrailingIcon: LogoutIcon, onClick: () => { /* TODO: logout */}},
+                    {children: "Профиль", TrailingIcon: UserIcon, onClick: () => onProfileClick(userId), isActive: isCorrectLocation(PROFILE_ROUTE(userId), location)},
+                    {children: "Друзья", TrailingIcon: FriendsIcon, onClick: onFriendsClick, isActive: isCorrectLocation(FRIENDS_ROUTE, location)},
+                    {children: "Настройки", TrailingIcon: SettingsIcon, onClick: () => { /* TODO: settings */ }, isActive: false }, // TODO path check
+                    {children: "Выйти из аккаунта", TrailingIcon: LogoutIcon, onClick: () => { /* TODO: logout */}, isActive: false}, // TODO path check
                 ]}/>
             }
         </div>
