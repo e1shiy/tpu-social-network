8dc46bdc3f2e7c2c91bf06a5a5c2b9f91ec551a0
1eshiy <1eshiy@vk.com>
2025-12-15 14:02:18 +0700

    fix gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss(),
-      svgr({
-      svgrOptions: {
-        dimensions: false, // отключает добавление width и height
-        svgProps: {
-          // "stroke-width": "2"
-        }
-      },
-    })
-  ],
+    base: "/tpu-social-network/",
+    plugins: [
+        react(),
+        tailwindcss(),
+        svgr({
+            svgrOptions: {
+                dimensions: false, // отключает добавление width и height
+                svgProps: {
+                    // "stroke-width": "2"
+                }
+            },
+        })
+    ],
 })
