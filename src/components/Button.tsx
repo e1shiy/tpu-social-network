eb18a9bbfae3610708696def48705164c7d847d0
1eshiy <1eshiy@vk.com>
2026-01-16 20:00:22 +0700

    add IconButton.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,12 +11,12 @@ export interface ButtonProps {
     LeadingIcon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }
 
-const buttonBaseStyles = `
-    h-[2.1875rem] sm:h-[2.3rem] md:h-[2.5rem] lg:h-[2.65rem] xl:h-[2.8rem] 2xl:h-[3rem] 
-    p-[0.625rem] sm:p-[0.675rem] md:p-[0.725rem] lg:p-[0.775rem] xl:p-[0.825rem] 2xl:p-[0.875rem] 
-    flex-center gap-[0.3125rem] md:gap-[0.625rem]
-    rounded-[0.3125rem] text-light font-bold cursor-pointer active:opacity-60 focus:outline-none
-`
+const buttonBaseStyles = twMerge(clsx(
+    "h-[2.1875rem] sm:h-[2.3rem] md:h-[2.5rem] lg:h-[2.65rem] xl:h-[2.8rem] 2xl:h-[3rem]",
+    "p-[0.625rem] sm:p-[0.675rem] md:p-[0.725rem] lg:p-[0.775rem] xl:p-[0.825rem] 2xl:p-[0.875rem]",
+    "flex-center gap-[0.3125rem] md:gap-[0.625rem]",
+    "rounded-[0.3125rem] text-light font-bold cursor-pointer active:opacity-60 focus:outline-none"
+))
 
 function Button({className, children, color = "primary", isActive=false, LeadingIcon, TrailingIcon, onClick}: ButtonProps) {
     const colorStyles = color === "primary"
---
+++ src/components/IconButton.tsx
@@ -0,0 +1,38 @@
+import {twMerge} from "tailwind-merge";
+import clsx from "clsx";
+
+interface IconButtonProps {
+    className?: string,
+    Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+    color?: keyof typeof colorSchemes;
+    isActive?: boolean;
+    onClick?: () => void;
+}
+
+const baseStyles = twMerge(clsx(
+    "h-3.5 md:h-4 lg:h-5 2xl:h-6",
+    "text-dark cursor-pointer active:opacity-60 focus:outline-none"
+))
+
+const colorSchemes = {
+    "default": (isActive: boolean) => isActive ? "text-dark" : "text-dark/60 hover:text-dark",
+    "primary": (isActive: boolean) => isActive ? "text-primary" : "text-primary hover:text-primary-alt",
+    "error": (isActive: boolean) => isActive ? "text-error" : "text-error hover:text-error-alt"
+}
+
+function IconButton({className, Icon, color="default", isActive=false, onClick} : IconButtonProps) {
+    const colorStyles = colorSchemes[color]
+
+    return(
+        <Icon
+            className={twMerge(clsx(
+                baseStyles,
+                colorStyles(isActive),
+                className
+            ))}
+            onClick={onClick}
+        />
+    )
+}
+
+export default IconButton
