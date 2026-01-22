31c5ed059f8c376117cf91aa2631ce30e36d9a9c
1eshiy <1eshiy@vk.com>
2026-01-22 18:55:37 +0700

    add pause on hover to pop-ups

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -2,10 +2,10 @@ import Card from "./wrappers/Card.tsx";
 import InfoIcon from "../assets/images/info.svg?react"
 import ErrorIcon from "../assets/images/error.svg?react"
 import SuccessIcon from "../assets/images/accept.svg?react"
-import {useCallback, useEffect} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import {twMerge} from "tailwind-merge";
 import clsx from "clsx";
-import {motion} from "framer-motion";
+import {motion, useAnimation} from "framer-motion";
 
 interface PopUpProps {
     id: number,
@@ -32,31 +32,49 @@ const IconColorVariants: { [K in keyof typeof IconVariants]: string } = {
     "success": "text-primary"
 } as const
 
-const lifetime = 5000
-
 function PopUp({children, status, onClose, id}: PopUpProps) {
     const Icon = IconVariants[status]
     const iconColor = IconColorVariants[status]
     const timelineColor = timelineColorVariants[status]
 
+    const [isPaused, setIsPaused] = useState(false)
+    const animation = useAnimation()
+
     const handleClose = useCallback(() => onClose(id), [id, onClose])
+    const lifetime = useRef<number>(5000)
 
     useEffect(() => {
-        const timeout = setTimeout(handleClose, lifetime)
-        return () => clearTimeout(timeout)
-    }, [handleClose])
+        if (isPaused) {
+            animation.stop()
+            return
+        }
+        animation.start({
+            width: 0,
+            transition: {
+                duration: lifetime.current / 1000,
+                ease: "linear"
+            }
+        })
+        const startTime = Date.now()
+        const timeout = setTimeout(handleClose, lifetime.current)
+        return () => {
+            clearTimeout(timeout)
+            lifetime.current -= Date.now() - startTime
+        }
+    }, [handleClose, isPaused])
 
     return (
         <Card
             className={"flex-center gap-2 lg:gap-3 relative select-none cursor-pointer max-w-[75vw] lg:max-w-[33vw]"}
             onClick={handleClose}
+            onMouseEnter={() => setIsPaused(true)}
+            onMouseLeave={() => setIsPaused(false)}
         >
             {children}
             <Icon className={twMerge(clsx("h-4 md:h-4.5 xl:h-5 shrink-0", iconColor))}/>
             <motion.span
                 initial={{width: "90%"}}
-                animate={{width: 0}}
-                transition={{ease: "linear", duration: lifetime / 1000}}
+                animate={animation}
                 className={twMerge(clsx(
                     "absolute left-1/2 -translate-x-1/2 bottom-1.5 h-0.5 lg:h-1 rounded-full", timelineColor
                 ))}
